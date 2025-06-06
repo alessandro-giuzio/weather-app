a228ec32ed1098d18661ca16648bc49e452ebd06
Alessandro Giuzio <38163055+alessandro-giuzio@users.noreply.github.com>
2025-06-06 17:21:35 +0200

    feat: update dependencies and configure Tailwind CSS
    - Added Tailwind CSS and Vue.js dependencies to package.json.
    - Updated Layout.astro to include global styles and changed the language to Spanish.
    - Enhanced tsconfig.json with path aliases for easier imports.
    - Created global.css to import Tailwind CSS styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components',
+        '@assets': '/src/assets',
+        '@styles': '/src/styles',
+        '@layouts': '/src/layouts',
+      },
+    },
+    plugins: [tailwindcss()],
+  },
+});
